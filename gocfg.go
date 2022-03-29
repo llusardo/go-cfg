7b4c5b0c07c3f46988118a4454b8c13e5007aba0
llusardo <lucaslusardo@gmail.com>
2022-03-29 19:52:36 -0300

    Second commit

--- gocfg.go
+++ gocfg.go
@@ -1,5 +1,27 @@
 package gocfg
 
-func Hello() string {
-  return "Hello, You!"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// ReadConfiguration unmarshalls the content of "env_{env} file in out struct pointer"
+func ReadConfiguration(env string, out interface{}) error {
+	file, err := os.Open(fmt.Sprintf("env_%s.json", env))
+
+	if err == nil {
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(out)
+	}
+	return err
+}
+
+// MySQL connection representation
+type MySQL struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Server   string `json:"server"`
+	Port     int    `json:"port"`
+	Db       string `json:"db"`
 }
